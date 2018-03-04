fad02dd832d35fa1cf08f8e6c5ac1f0f2fd65af2
Phuong Nam Nguyen <luckyadvancement@gmail.com>
2018-03-03 18:41:38 -0800

    Added surface between path and simulated path
    - Geo coordinates are taken into account when computing distance now

--- pathplan/pathtools.py
+++ pathplan/pathtools.py
@@ -7,15 +7,23 @@ around 1 usually.
 
 NOTE: This file uses Generators, Lists, Numpy Arrays interchangely, but
 will do conversions from generators to lists to numpy arrays if necessary.
+
+NOTE: This code was written using Python 3 so Python 2 will probably cause
+some errors with generators in this file.
 """
+
 from mpl_toolkits.mplot3d import Axes3D
 
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import pyproj
+import sys
+import math
 
 import types
 
+wgs84 = pyproj.Proj(init="epsg:4326")
 
 def generator_to_list(array):
     if isinstance(array, types.GeneratorType):
@@ -26,6 +34,64 @@ def to_np_array(array):
     if not isinstance(array, np.ndarray):
         return np.array(array)
     return array
+
+# TODO: Move this to a commons file. This was copypasted from path_planner.py
+# which was (at the time) in python2, so the file couldn't be imported.
+def utm_zone(lat, lon):
+    """Determine the UTM zone for a given latitude and longitude.
+
+    Based on http://gis.stackexchange.com/a/13292
+
+    Args:
+        lat: Latitude
+        lon: Longitude
+
+    Returns:
+        zone number: UTM zone number lat/lon falls in
+        north: bool indicating North/South
+    """
+    zone = math.floor((lon + 180) / 6.0) + 1
+
+    # Special cases for Norway and Svalbard
+    if lat >= 56 and lat < 64 and lon >= 3 and lon < 12:
+        zone = 32
+
+    if lat >= 72 and lat < 84:
+        if lon >= 0 and lon < 9:
+            zone = 31
+        elif lon >= 9 and lon < 21:
+            zone = 33
+        elif lon >= 21 and lon < 33:
+            zone = 35
+        elif lon >= 33 and lon < 42:
+            zone = 37
+
+    return zone, lat > 0
+
+
+def proj_utm(zone, north):
+    """Proj instance for the given zone.
+
+    Args:
+        zone: UTM zone
+        north: North zone or south zone
+
+    Returns:
+        pyproj.Proj instance for the given zone
+    """
+    ref = "+proj=utm +zone=%d +ellps=WGS84" % zone
+    if not north:
+        ref += " +south"
+    return pyproj.Proj(ref)
+
+
+def utm_proj(lat, lon):
+    """
+    Returns the utm_proj for the given lat and lon
+    """
+
+    zone, north = utm_zone(lat, lon)
+    return proj_utm(zone, north)
 
 
 
@@ -40,9 +106,11 @@ def read_path_from_json(filepath):
     Y = "latitude"
     Z = "altitude"
 
-    to_xyz = lambda pt: np.array([pt[X], pt[Y], pt[Z]])
+    proj      = lambda pt: utm_proj(pt[X], pt[Y])
+    cartesian = lambda pt: pyproj.transform(wgs84, proj(pt), pt[X], pt[Y], pt[Z])
+    xyz       = lambda pt: np.array([*cartesian(pt)])
     points = json.load(open(filepath))
-    return map(to_xyz, points)
+    return map(xyz, points)
 
 def default_noise(val=0):
     return val + np.random.normal(0, 1.5)
@@ -92,6 +160,29 @@ def get_dist_between_points(points, scale=1):
 def total_dist(path):
     return sum(get_dist_between_points(path))
 
+def get_nearest_point_from(pt, list_of_points, set):
+    # NOTE: Replace with octree/kd-tree for better performance in future:
+    minlen = sys.float_info.max
+    minval = None
+    for other in list_of_points:
+        if tuple(other) in set:
+            continue
+
+        length = norm(pt - other)
+        if length < minlen:
+            minlen = length
+            minval = other
+
+    return minval
+
+def gen_path_via_nearest_points(planned, flown):
+    used_pts = set()
+    for pt in planned:
+        found_pt = get_nearest_point_from(pt, flown, used_pts)
+        used_pts.add(tuple(found_pt))
+        yield found_pt
+
+
 def mse(expected, actual):
     """
     Mean squared error of expected and actual waypoints.
@@ -115,8 +206,8 @@ def print_planned_and_flown_path_debug_info(planned, flown, metric=mse):
     print(f"Path Debug:")
     print(f"  len(planned) = {len(planned)}")
     print(f"  len(flown)   = {len(flown)}")
-    print(f"  Total distance traveled for planned path: {total_dist(planned)}")
-    print(f"  Total distance traveled for flown path: {total_dist(planned)}")
+    print(f"  Planned Path Total distance: {total_dist(planned)}")
+    print(f"  Flown Path Total distance:   {total_dist(flown)}")
     print(f"  Error based on metric = {metric(planned, flown)}")
 
 def display_two_paths(one, two):
@@ -179,11 +270,16 @@ def display_surface_with_file(filepath):
 
 
 def main():
-    err = calc_errors_with_gen_noise("output/path.json", metric=mse)
-    print("MSE={}".format(err))
+    planned = list(read_path_from_json("output/min_alt_2_gps.json"))
+    flown = read_path_from_json("output/min_alt_2.flight.json")
 
-    # display_gen_noise_path_with_file("path.json")
-    display_surface_with_file("output/path.json")
+    # NOTE: altitude in output/min_alt_2.flight.json adds 584
+    flown = list(map(lambda xyz: np.array([xyz[0], xyz[1], xyz[2] - 584.0]), flown))
+    flown = list(gen_path_via_nearest_points(planned, flown))
+
+    print_planned_and_flown_path_debug_info(planned, flown)
+    display_surface(planned, flown)
+
 
 # Uncomment to test
 # if __name__ == "__main__":
